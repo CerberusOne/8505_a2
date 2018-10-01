de3c960e9fd8297b0c44036f51e9711a5c42a38b
Ben <bl_04@hotmail.com>
2018-09-30 21:51:56 -0700

    working encryption and file transfer

--- stego.c
+++ stego.c
@@ -22,11 +22,13 @@
 #include "./wrappers/socketwrappers.h"
 
 static struct option long_options[] = {
-    {"ip",      required_argument, 0, 'i'},
-    {"port",    required_argument, 0, 'p'},
-    {"file",    required_argument, 0, 'f'},
-    {"server",  no_argument, 0, 's'},
-    {0,         0,                 0, 0}
+    {"address",          required_argument, 0, 'a'},
+    {"port",             required_argument, 0, 'p'},
+    {"encrypt",          no_argument,       0, 'e'},
+    {"server",           no_argument,       0, 's'},
+    {"stest",            no_argument,       0, 'd'},
+    {"ctest",            no_argument,       0, 'c'},
+    {0,                  0,                 0,   0}
 };
 
 #define print_usage() \
@@ -39,53 +41,62 @@ static struct option long_options[] = {
                 "");\
     }while(0)
 
-#define BUFFLEN 1024
+#define BUFFLEN 65536
 #define MAXEVENTS 60
 
+
+void crypto(char *input,char *output, unsigned char *key, unsigned char *iv, bool encryptfile);
 int sendFile(int serversocket, int input, int pipefd[2]);
 void NewConnection(int socket, int epollfd);
 void spliceTo(int source, int destination, int pipefd[2]);
-//int decrypt(FILE *input, FILE *output, unsigned char *key, unsigned char *iv);
-//int encrypt(FILE *input, FILE *output, unsigned char *key, unsigned char *iv);
-//void HandleErrors(void);
+int decrypt(FILE *input, FILE *output, unsigned char *key, unsigned char *iv);
+int encrypt(FILE *input, FILE *output, unsigned char *key, unsigned char *iv);
+void HandleErrors(void);
 
 int main(int argc, char **argv){
-    //FILE* input;
-    //FILE* output;
-    int output;
+    unsigned char *key = (unsigned char *)"01234567890123456789012345678901"; //Key
+    unsigned char *iv = (unsigned char*)"0123456789012345"; //IV
     struct epoll_event event;
     struct epoll_event *events;
-    int c, listensocket, epollfd;
+    int arg, listensocket, epollfd;
     bool server = false;
-    char * ip = 0;
-    char * port = 0;
-    char *filename;
+    char *address = 0;
+    char *port = 0;
+    char *inputfile;
+    char *outputfile;
     int pipefd[2];
+    int servertest, clienttest;
+    int encrypt = true;
 
     while(1){
         int option_index = 0;
-        c = getopt_long(argc, argv, "i:p:s:f", long_options, &option_index);
-        if(c == -1){
+        arg = getopt_long(argc, argv, "i:p:f:e:s:d:c", long_options, &option_index);
+        if(arg == -1){
             break;
         }
-        switch(c){
+        switch(arg){
             case 'i':
-                ip = optarg;
-                printf("Destination Address: %s\n", ip);
+                address = optarg;
+                printf("Destination Address: %s\n", address);
                 break;
             case 'p':
                 port = optarg;
                 printf("Port: %s\n", port);
                 break;
-            case'f':
-                filename = optarg;
-                printf("Filename: %s\n", filename);
+            case 'e':
+                encrypt = false;
+                printf("Encrypt: true\n");
                 break;
-            case's':
+            case 's':
                 server = true;
                 printf("Server: true\n");
                 break;
-            case '?':
+            case 'd':
+                servertest = true;
+                break;
+            case 'c':
+                servertest = false;
+                break;
 
             default:
                 //print_usage();
@@ -93,43 +104,15 @@ int main(int argc, char **argv){
         }
     }
 
-    //initilize key
-    //unsigned char *key = (unsigned char *)"01234567890123456789012345678901";
-    //initialization vector
-    //unsigned char *iv = (unsigned char*)"0123456789012345";
-
-    /*
-       if((input = fopen("Encrypted_file.txt", "rb")) == NULL){
-       printf("Could not be open for reading \n");
-       exit(1);
-       }
-
-       if((output = fopen("Decrypted_file.txt", "wb")) == NULL){
-       printf("Encrypted_file could not be opened for writing");
-       exit(1);
-       }
-       server = 1;
-       if(server){
-       decrypt(input, output, key, iv);
-       } else{
-       encrypt(input, output, key, iv);
-       }*/
 
     if(pipe(pipefd) == -1) {
         perror("creating pipe");
     }
 
     if(server){
-        /*
-        if((output = fopen("output.bmp", "wb+")) == NULL){
-            perror("fopen");
-        }
-        */
-
-        if((output = open("output.bmp", O_WRONLY)) < 0) {
-            perror("file can't be opened");
-            exit(1);
-        }
+        inputfile = "output";
+        outputfile = "output.bmp";
+        int outputfd;
 
 
         listensocket = makeBind(port);
@@ -154,52 +137,40 @@ int main(int argc, char **argv){
                     NewConnection(events[i].data.fd, epollfd);
                     printf("New Connection on the server!\n");
                 } else if(events[i].events & EPOLLIN){
-                    //NewData(events[i].data.fd, output, pipefd);
-                    spliceTo(events[i].data.fd, output, pipefd);
+                    if((outputfd = open(inputfile, O_WRONLY)) < 0) {
+                        perror("file can't be opened");
+                        exit(1);
+                    }
                     printf("New data!\n");
+                    spliceTo(events[i].data.fd, outputfd, pipefd);
+                    close(outputfd);
+                    crypto(inputfile, outputfile , key, iv, false);
                 }
             }
         }
-
-        close(output);
     } else {
         int serversocket;
-        int input;
+        int inputfd;
+        inputfile = "input.bmp";
+        outputfile = "input";
 
-        if((input = open("linux.bmp", O_RDONLY)) < 0) {
+
+        if(encrypt){
+            crypto(inputfile, outputfile, key, iv, true);
+        } else {
+
+        }
+
+        if((inputfd = open(outputfile, O_RDONLY)) < 0) {
             perror("file can't be opened");
             exit(1);
         }
 
-        serversocket = makeConnect(ip, port);
-        //sendFile(serversocket, input, pipefd);
-        spliceTo(input, serversocket, pipefd);
+        serversocket = makeConnect(address, port);
+        spliceTo(inputfd, serversocket, pipefd);
 
-        close(input);
+        close(inputfd);
     }
-
-    //initilize key
-    //unsigned char *key = (unsigned char *)"01234567890123456789012345678901";
-    //initialization vector
-    //unsigned char *iv = (unsigned char*)"0123456789012345";
-
-    /*
-       if((input = fopen("Encrypted_file.txt", "rb")) == NULL){
-       printf("Could not be open for reading \n");
-       exit(1);
-       }
-
-       if((output = fopen("Decrypted_file.txt", "wb")) == NULL){
-       printf("Encrypted_file could not be opened for writing");
-       exit(1);
-       }
-       server = 1;
-       if(server){
-       decrypt(input, output, key, iv);
-       } else{
-       encrypt(input, output, key, iv);
-       }*/
-
     return 0;
 }
 
@@ -214,7 +185,7 @@ void spliceTo(int source, int destination, int pipefd[2]){
             perror("getting splice error");
         }
 
-        if(getBytes == 0) {
+        if(getBytes <= 0) {
             return;
         }
 
@@ -258,6 +229,27 @@ void NewConnection(int socket, int epollfd){
         event.data.fd = clientsocket;
         addEpollSocket(epollfd, clientsocket, &event);
     }
+}
+void crypto(char *input,char *output, unsigned char *key, unsigned char *iv, bool encryptfile){
+    FILE* cryptoinput;
+    FILE* cryptooutput;
+    if((cryptoinput = fopen(input, "rb")) == NULL){
+       printf("Could not be open for reading \n");
+       exit(1);
+    }
+
+    if((cryptooutput = fopen(output, "wb")) == NULL){
+        printf("Could not be opened for writing");
+        exit(1);
+    }
+    if(encryptfile){
+        encrypt(cryptoinput, cryptooutput, key, iv);
+    } else {
+        decrypt(cryptoinput, cryptooutput, key, iv);
+    }
+    fclose(cryptoinput);
+    fclose(cryptooutput);
+
 }
 
 void handleErrors(void){
@@ -313,6 +305,7 @@ int decrypt(FILE *input, FILE *output, unsigned char *key, unsigned char *iv){
     unsigned char inbuff[BUFFLEN];
     unsigned char outbuff[BUFFLEN];
     int bytesRead, bytesWritten, outLen;
+    int result;
     //initilize context
     if(!(ctx = EVP_CIPHER_CTX_new())){
         handleErrors();
@@ -327,7 +320,7 @@ int decrypt(FILE *input, FILE *output, unsigned char *key, unsigned char *iv){
             printf("Could not read file");
             exit(1);
         }
-        if((EVP_DecryptUpdate(ctx, outbuff, &outLen, inbuff, bytesRead)) != 1){
+        if((result = EVP_DecryptUpdate(ctx, outbuff, &outLen, inbuff, bytesRead)) != 1){
             handleErrors();
         }
 
